f29a45143d9ce305d58ae9e5170c1d7f981bec5b
nelsonic <contact.nelsonic+github@gmail.com>
2017-12-10 22:33:26 +0000

    split code from Main.elm in several smaller modules for modularity ... (duh!) see: https://www.elm-tutorial.org/en/04-starting/07-multiple-modules.html

--- src/Main.elm
+++ src/Main.elm
@@ -1,45 +1,28 @@
+module Main exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (program)
+import Msgs exposing (Msg)
+import Models exposing (Model, initialModel)
+import Update exposing (update)
+import View exposing (view)
 
--- MODEL
-
-type alias Model = String
 
 init : ( Model, Cmd Msg )
 init =
-    ( "Hello", Cmd.none )
+    ( initialModel, Cmd.none )
 
--- MESSAGES
-
-type Msg
-    = NoOp
-
--- VIEW
-
-view : Model -> Html Msg
-view model = 
-    div []
-        [ text model ]
-
--- UPDATE
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model = 
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
--- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
 
+
 -- MAIN
 
+
 main : Program Never Model Msg
-main = 
+main =
     program
         { init = init
         , view = view
---
+++ src/Models.elm
@@ -0,0 +1,23 @@
+module Models exposing (..)
+
+
+type alias Model =
+    { players : List Player
+    }
+
+
+initialModel : Model
+initialModel =
+    { players = [ Player "1" "Sam" 1 ]
+    }
+
+
+type alias PlayerId =
+    String
+
+
+type alias Player =
+    { id : PlayerId
+    , name : String
+    , level : Int
+    }
